b54e713198d80cd1eb18c702c0391dd75ad1f8f0
iOS Apps Development <info@iosappsdev.org>
2014-06-03 23:17:18 -0700

    Initial Commit

---
+++ MasterDetail/Detail2VC.h
@@ -0,0 +1,13 @@
+//
+//  Detail2VC.h
+//  MasterDetail
+//
+//  Created by CtecTeacher on 6/3/14.
+//  Copyright (c) 2014 iOS Apps Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Detail2VC : UIViewController
+
+@end
---
+++ MasterDetail/Detail3VC.h
@@ -0,0 +1,13 @@
+//
+//  Detail3VC.h
+//  MasterDetail
+//
+//  Created by CtecTeacher on 6/3/14.
+//  Copyright (c) 2014 iOS Apps Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Detail3VC : UIViewController
+
+@end
